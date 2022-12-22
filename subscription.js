3f03eaadb8ce9a1e6db529caf2e95f482a4baeb9
hitensinha <47480336+hitensinha@users.noreply.github.com>
2022-12-22 12:11:55 +0530

    Update subscription.js
    added new ui with api integration

--- subscription.js
+++ subscription.js
@@ -1,35 +1,36 @@
 var API_URL = "https://api.timelessart.io/api/timelessart/fetchSubs";
-var colors = ['#cff1ec', '#ecf1e0', '#ffe1e1', '#f0eaea', 'red'];
+var IMAGE_URL = "https://uploads-ssl.webflow.com/629e3d15e8ee4837214bee58/63a14b4ea97b93538ec5c797_subscription-p-500.jpeg"; 
 
-$(window).on('load', function () {
+$(document).ready(function () {
 
     $.get(API_URL, function (response) {
         if (response && response.message && response.message.data) {
             var list = response.message.data;
             var total_plans = list.length;
-            var columnSize = 12 / total_plans;
+            var columnSize = 12 / total_plans; 
+             
+            var list = list.sort((a, b) => parseFloat(a.amount) - parseFloat(b.amount));
 
             var finalHTML = "";
-            list.forEach(function (item, index) {
+            list.forEach(function (item, index) { 
                 var planName = item.nickname;
                 var metaData = item.metadata;
                 var planUrl = item.url;
                 var planAmount = item.amount / 100;
-                var buttonText = "Current Plan";
-
+                var buttonText = "PURCHASE NOW";
+                
                 var columnHtml = getColumnHTML(columnSize);
-
                 columnHtml = columnHtml.replace("#PLANNAME#", planName);
                 columnHtml = columnHtml.replace("#PLANAMOUNT#", planAmount);
+                columnHtml = columnHtml.replace("#PLANIMAGEURL#", IMAGE_URL);
                 columnHtml = columnHtml.replace("#PLANURL#", planUrl);
-                columnHtml = columnHtml.replace("#BACKGROUNDCOLOR#", colors[index]);
                 columnHtml = columnHtml.replace("#COLUMNSIZE#", columnSize);
                 columnHtml = columnHtml.replace("#BUTTONTEXT#", buttonText);
 
-                finalHTML += columnHtml;
-            });
+                finalHTML += columnHtml; 
+            }); 
 
-            $(".w-row").html(finalHTML);
+            $(".planlist").html(finalHTML);
         }
     });
 
@@ -37,21 +38,16 @@ $(window).on('load', function () {
 
 function getColumnHTML() {
     var htmlData = "";
-    htmlData += '<div class="w-col w-col-#COLUMNSIZE#" style="height:500px; background-color:#BACKGROUNDCOLOR#">';
-    htmlData += '<h3 class="heading-3">#PLANNAME#</h3>';
-    htmlData += '<div class="w-row">';
-    htmlData += '<div class="w-col w-col-12">';
-    htmlData += '<div class="text-block" style="text-align:center;">$#PLANAMOUNT# <span style="font-size:18px; font-weight: bold;">/mo</span></div>';
-    htmlData += '</div>';
-    htmlData += '</div>';
-
-    htmlData += '<div class="clsMetadata">';
-    htmlData += '<div class="text-block-3">100 requests monthly</div>';
-    htmlData += '<div class="text-block-3">No Rate Limit</div>';
-    htmlData += '<div class="text-block-3">No credit card required</div>';
-    htmlData += '</div>';
-
-    htmlData += '<div class="section-2 wf-section"><a href="#PLANURL#" target="_blank" class="button-3 w-button">#BUTTONTEXT#</a></div>';
+    htmlData += '<div class="pricing-card-2">';
+    htmlData += '<img src="#PLANIMAGEURL#" loading="lazy" height="180" width="320" alt="Plan Image" class="image-2" />';
+    htmlData += '<h2 class="pricing-title-2" style="font-size: 13px; line-height: 25px; text-align: center;">#PLANNAME#</h2>';
+    htmlData += '<div class="pricing-price-2" style="padding-top: 20px; margin-bottom: 80px;">$#PLANAMOUNT#</div>';
+    htmlData += '<div class="paragraph-regular-2 margin-bottom-20">World Class Curators</div>';
+    htmlData += '<div class="paragraph-regular-2 margin-bottom-20">25 Art listing</div>';
+    htmlData += '<div class="paragraph-regular-2 margin-bottom-20">60 minutes complimentary</div>';
+    htmlData += '<h1 class="heading-5">Timeless</h1>';
+    htmlData += '<div class="pricing-divider-2"></div>';
+    htmlData += '<a href="#PLANURL#" target="_blank" class="button-primary-2 outline-button w-button">#BUTTONTEXT#</a>';
     htmlData += '</div>';
     return htmlData;
 }
